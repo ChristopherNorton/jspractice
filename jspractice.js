c774a0cedb46f98b17370d63dfa9e0682237d8ea
ChristopherNorton <46770597+ChristopherNorton@users.noreply.github.com>
2019-02-20 21:50:02 -0800

    Add files via upload

---
+++ jspractice.js
@@ -0,0 +1,21 @@
+
+  var arr1 = [3,9,15,4,10]
+  function times3(array) {
+      var newArr = []
+      for(let i = 0; i < array.length; i++) {
+          newArr.push(array[i] * 3)
+      }
+      return newArr
+  }
+
+  console.log(times3(arr1))
+
+  var arr1 = [3, 9, 15, 4, 10]
+  function timesThree (array) {
+    var newArr = []
+    for (let i = 0; i < array.length; i++) {
+      newArr.push(array[i] * 3)
+    }
+    return newArr
+  }
+    console.log(timesThree(arr1))
